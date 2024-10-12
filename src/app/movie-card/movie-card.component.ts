b2b7d83b3f54edc6a5e499135d974bf0ab830876
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-10-12 19:24:37 +0200

    Refactor movie card layout: integrate Bootstrap for responsive design, enhance styling, and add director information

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule, NgForOf } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { NgForOf, AsyncPipe } from '@angular/common';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MovieThumbnailPipe } from '../movie-thumbnail.pipe';
 import { Movie } from '../model/movie.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MDCRipple } from '@material/ripple';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-movie-card',
@@ -14,14 +17,16 @@ import { Movie } from '../model/movie.model';
         MatDialogModule,
         NgForOf,
         MovieThumbnailPipe,
-        CommonModule
+        AsyncPipe,
+        MatButtonModule,
+        MatIconModule
     ],
     templateUrl: './movie-card.component.html',
     styleUrl: './movie-card.component.scss'
 })
-export class MovieCardComponent implements OnInit {
+export class MovieCardComponent implements OnInit, AfterViewInit {
     movies: Movie[] = [];
-    constructor(public fetchApiData: FetchApiDataService) {
+    constructor(public fetchApiData: FetchApiDataService, private el: ElementRef) {
     }
     ngOnInit(): void {
         const subscriber = this.fetchApiData.getMovies().subscribe({
@@ -35,5 +40,10 @@ export class MovieCardComponent implements OnInit {
                 subscriber.unsubscribe
             }
         });
+    }
+    ngAfterViewInit(): void {
+        for (let button of this.el.nativeElement.querySelectorAll('button')) {
+            new MDCRipple(button);
+        }
     }
 }
