69a5d83d6268339c12779374f7d3989c12b7705b
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-10-12 15:58:39 +0200

    Revert "Revert "Added AWS S3" because AWS secrets should not be part of a repo"
    This reverts commit bbff1e45717f80dfd59fa9b39b9451cc0f47e182.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,7 @@
 export const environment = {
-    apiUrl: 'https://oles-myflix-810b16f7a5af.herokuapp.com/'
+    apiUrl: 'https://oles-myflix-810b16f7a5af.herokuapp.com/',
+    S3_BUCKET: 'freshmozartsmyflix',
+    S3_REGION: 'eu-central-1',
+    AWS_ID: 'AKIAT2J4YPISRUEEFJMO',
+    AWS_SECRET: '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'
 };
