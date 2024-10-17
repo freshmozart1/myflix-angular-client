47893c0546bad3cb966d599b0b43d88080de458a
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-10-17 13:03:55 +0200

    Refactor user authentication handling in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserService } from './user.service';
 
@@ -14,14 +14,11 @@ import { UserService } from './user.service';
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
-    protected user: UserService['user'] | null; //user is either of type 'user' from UserService or null
     constructor(
         protected userService: UserService
-    ) {
-        this.user = this.userService.user;
-    }
+    ) { }
+
     protected logout() {
         this.userService.user = null;
-        this.user = null;
     }
 }
