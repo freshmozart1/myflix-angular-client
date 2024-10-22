f4bb44d7fea6da408b17f9957f5f07115c237af2
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-10-22 16:51:50 +0200

    Update environment variable logging to display lengths instead of values for security

--- set-env.js
+++ set-env.js
@@ -3,11 +3,11 @@ import { writeFile } from "fs";
 const targetPath = "./src/environments/environment.ts";
 console.log('==================================');
 console.log('Generating environment.ts file with the following environment variables:');
-console.log('API_URL:', process.env['API_URL']);
-console.log('S3_BUCKET:', process.env['S3_BUCKET']);
-console.log('S3_REGION:', process.env['S3_REGION']);
-console.log('AWS_ID:', process.env['AWS_ID']);
-console.log('AWS_SECRET:', process.env['AWS_SECRET']);
+console.log('API_URL:', process.env['API_URL'].length);
+console.log('S3_BUCKET:', process.env['S3_BUCKET'].length);
+console.log('S3_REGION:', process.env['S3_REGION'].length);
+console.log('AWS_ID:', process.env['AWS_ID'].length);
+console.log('AWS_SECRET:', process.env['AWS_SECRET'].length);
 console.log('==================================');
 
 const envConfigFile = `export const environment = {
